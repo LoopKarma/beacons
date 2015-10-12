01614a5712c048344c7fd78179b9d0135e9e1b5a
Karma <originalzed@gmail.com>
2015-10-12 16:42:53 +0300

    Migrate fix

--- migrations/m151008_182726_templateAddings.php
+++ migrations/m151008_182726_templateAddings.php
@@ -13,12 +13,18 @@ class m151008_182726_templateAddings extends Migration
         $this->addColumn(
             $this->tableName,
             'send_unlimited',
-            $this->smallInteger(1)->defaultValue(0) . ' AFTER barcode_message_encoding'
+        $this->smallInteger(1)->defaultValue(0) . ' AFTER barcode_message_encoding'
         );
 
+        $this->addColumn(
+            $this->tableName,
+            'icon',
+            $this->string(10)->defaultValue(null) . ' AFTER send_unlimited'
+        );
         $this->addColumn($this->tableName, 'icon2x', $this->string(10)->defaultValue(null) . ' AFTER icon');
         $this->addColumn($this->tableName, 'icon3x', $this->string(10)->defaultValue(null) . ' AFTER icon2x');
 
+        $this->addColumn($this->tableName, 'logo', $this->string(10)->defaultValue(null) . ' AFTER icon3x');
         $this->addColumn($this->tableName, 'logo2x', $this->string(10)->defaultValue(null) . ' AFTER logo');
         $this->addColumn($this->tableName, 'logo3x', $this->string(10)->defaultValue(null) . ' AFTER logo2x');
 
@@ -29,11 +35,13 @@ class m151008_182726_templateAddings extends Migration
 
         $this->addColumn($this->tableName, 'name', $this->string(50)->notNull() . ' AFTER template_id');
 
+        /*
         $this->dropColumn($this->tableName, 'icon_retina');
         $this->dropColumn($this->tableName, 'logo_retina');
         $this->dropColumn($this->tableName, 'strip_image');
         $this->dropColumn($this->tableName, 'strip_image_retina');
         $this->dropColumn($this->tableName, 'barcode_visible');
+        */
     }
 
     public function down()
