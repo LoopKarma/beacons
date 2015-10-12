fec01acb092c0f4b34acfe9c6c95fc2d2c708d9a
Karma <originalzed@gmail.com>
2015-10-12 16:38:02 +0300

    Migrate fix

--- migrations/m151008_182726_templateAddings.php
+++ migrations/m151008_182726_templateAddings.php
@@ -16,18 +16,18 @@ class m151008_182726_templateAddings extends Migration
             $this->smallInteger(1)->defaultValue(0) . ' AFTER barcode_message_encoding'
         );
 
-        $this->addColumn($this->tableName, 'icon2x', $this->string(10)->defaultValue(null) . 'AFTER icon');
-        $this->addColumn($this->tableName, 'icon3x', $this->string(10)->defaultValue(null) . 'AFTER icon2x');
+        $this->addColumn($this->tableName, 'icon2x', $this->string(10)->defaultValue(null) . ' AFTER icon');
+        $this->addColumn($this->tableName, 'icon3x', $this->string(10)->defaultValue(null) . ' AFTER icon2x');
 
-        $this->addColumn($this->tableName, 'logo2x', $this->string(10)->defaultValue(null) . 'AFTER logo');
-        $this->addColumn($this->tableName, 'logo3x', $this->string(10)->defaultValue(null) . 'AFTER logo2x');
+        $this->addColumn($this->tableName, 'logo2x', $this->string(10)->defaultValue(null) . ' AFTER logo');
+        $this->addColumn($this->tableName, 'logo3x', $this->string(10)->defaultValue(null) . ' AFTER logo2x');
 
-        $this->addColumn($this->tableName, 'strip', $this->string(10)->defaultValue(null) . 'AFTER logo3x');
-        $this->addColumn($this->tableName, 'strip2x', $this->string(10)->defaultValue(null) . 'AFTER strip');
-        $this->addColumn($this->tableName, 'strip3x', $this->string(10)->defaultValue(null) . 'AFTER strip2x');
+        $this->addColumn($this->tableName, 'strip', $this->string(10)->defaultValue(null) . '  AFTER logo3x');
+        $this->addColumn($this->tableName, 'strip2x', $this->string(10)->defaultValue(null) . ' AFTER strip');
+        $this->addColumn($this->tableName, 'strip3x', $this->string(10)->defaultValue(null) . ' AFTER strip2x');
 
 
-        $this->addColumn($this->tableName, 'name', $this->string(50)->notNull() . 'AFTER template_id');
+        $this->addColumn($this->tableName, 'name', $this->string(50)->notNull() . ' AFTER template_id');
 
         $this->dropColumn($this->tableName, 'icon_retina');
         $this->dropColumn($this->tableName, 'logo_retina');
