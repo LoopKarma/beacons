761ec007fd5b2880065585713bc426a1dcc1427f
Karma <originalzed@gmail.com>
2015-11-22 23:56:42 +0300

    Mail

--- config/params.php
+++ config/params.php
@@ -2,8 +2,7 @@
 
 return [
     'adminEmail' => 'dmitry.artamonov@getcouponapp.com',
-    //'sendEmail' => 'daart@list.ru',
-    'sendEmail' => 'originalzed@gmail.com',
+    'sendEmail' => 'daart@list.ru',
     'fromEmail' => 'noanswer@getcouponapp.com',
     'siteUrl' => 'getcouponapp.com',
     'adminPassword' => '123456',
